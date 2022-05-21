2ee58d002922012dc18409e4444b241ff698fe22
Benji Smith <6193112+Benjins@users.noreply.github.com>
2022-05-21 10:29:52 -0400

    daling with memset, maybe it works but for now we don't crash

--- src/parse_exe.rs
+++ src/parse_exe.rs
@@ -24,6 +24,22 @@ fn align_vec(vec : &mut Vec<u8>, alignment : usize) {
 	}
 }
 
+// TODO: Do the same for Linux/etc.
+
+#[cfg(target_os = "windows")]
+const MEMSET_x86_BYTES : [u8 ; 28] = [
+  0x48, 0x89, 0xc8,                    // mov    %rcx,%rax
+  0x4d, 0x85, 0xc0,                    // test   %r8,%r8
+  0x74, 0x13,                          // je     1b <?my_memset@@YAPEAXPEAXH_K@Z+0x1b>
+  0x31, 0xc9,                          // xor    %ecx,%ecx
+  0x66, 0x0f, 0x1f, 0x44, 0x00, 0x00,  // nopw   0x0(%rax,%rax,1)
+  0x88, 0x14, 0x08,                    // mov    %dl,(%rax,%rcx,1)
+  0x48, 0xff, 0xc1,                    // inc    %rcx
+  0x49, 0x39, 0xc8,                    // cmp    %rcx,%r8
+  0x75, 0xf5,                          // jne    10 <?my_memset@@YAPEAXPEAXH_K@Z+0x10>
+  0xc3                                 // retq
+];
+
 pub fn parse_obj_file(bin_data : &[u8], func_name : &str) -> Option<ExecPage> {
 	let obj_file = object::File::parse(bin_data).expect("");
 	
@@ -59,9 +75,18 @@ pub fn parse_obj_file(bin_data : &[u8], func_name : &str) -> Option<ExecPage> {
 			// However given that we're running this in our own process anyway, we kinda just assume that that doesn't happen
 			chk_stk_file_offset = Some(bytes_loaded_into_memory.len());
 			bytes_loaded_into_memory.push(0xc3);
+			break;
 		}
 	}
-	
+
+	let mut memset_file_offset = None;
+	for symbol in obj_file.symbols() {
+		if symbol.name().expect("") == "memset" {
+			memset_file_offset = Some(bytes_loaded_into_memory.len());
+			bytes_loaded_into_memory.extend_from_slice(&MEMSET_x86_BYTES[..]);
+		}
+	}
+
 	if let Some(section) = obj_file.section_by_name(".text") {
 		for symbol in obj_file.symbols() {
 			let symbol_name = symbol.name().expect("");
@@ -110,6 +135,23 @@ pub fn parse_obj_file(bin_data : &[u8], func_name : &str) -> Option<ExecPage> {
 										else {
 											panic!("bad relocation encoding");
 										}
+									}
+									else if target_symbol.name().expect("") == "memset" {
+										// TODO: Some code dup with above
+										let encoding = reloc.encoding();
+										let reloc_size = reloc.size() as usize;
+										if encoding == object::RelocationEncoding::Generic {
+											let reloc_offset_in_memory = (memset_file_offset.unwrap()) as i64;
+											let reloc_insert_offset_in_memory = (text_offset_in_memory + reloc_addr as usize) as i64;
+											// TODO: wait do we need the addend?
+											let addend = reloc.addend();
+											let reloc_relative_offset = reloc_offset_in_memory - reloc_insert_offset_in_memory + addend;
+											exec_page.fix_up_redirect(reloc_insert_offset_in_memory as usize, reloc_size, reloc_relative_offset, reloc.has_implicit_addend());
+										}
+										else {
+											panic!("bad relocation encoding");
+										}
+
 									}
 									else {
 										println!("symbol had no section index, and is not __chkstk {:?}", target_symbol);
@@ -189,35 +231,3 @@ pub fn parse_obj_file(bin_data : &[u8], func_name : &str) -> Option<ExecPage> {
 	return None;
 }
 
-
-
-
-
-
-//pub fn get_func_code_bytes(exe_buffer : &Vec<u8>, func_name : &str) -> Vec<u8> {
-//	let obj_file = object::File::parse(&**exe_buffer).unwrap();
-//	println!("obj_file = {:?}", obj_file);
-//	
-//	//if let Some(section) = obj_file.section_by_name(".boot") {
-//	//	println!("{:#x?}", section.data()?);
-//	//} else {
-//	//	eprintln!("section not available");
-//	//}
-//	
-//	return Vec::<u8>::new();
-//}
-
-//pub fn get_func_code_bytes(exe_buffer : &Vec<u8>, func_name : &str) -> Vec<u8> {
-//	match Object::parse(exe_buffer).unwrap() {
-//		Object::Elf(elf) => {
-//			println!("elf: {:#?}", &elf);
-//		},
-//		Object::PE(pe) => {
-//			println!("pe: {:#?}", &pe);
-//		},
-//		Object::Unknown(magic) => { panic!("unknown magic: {:#x}", magic) },
-//		_ => { panic!("Unknown exe file type"); }
-//	}
-//
-//	return Vec::<u8>::new();
-//}
